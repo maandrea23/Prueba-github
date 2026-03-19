ec365f9ed5e4e78ece0688abe488b21052074e75
Andres-1109 <felipe11092002@gmail.com>
2026-03-19 18:07:12 -0500

    Added balance to the functions to show total balance

--- src/services.py
+++ src/services.py
@@ -2,26 +2,33 @@
 from validations import validate_int_over_0, validate_not_empty
 #Creating provitional list to work the functions
 operations = []
+balance = 0
 #Function for income uses validations on the valitadions file and adds the values to a local dictionary to add this dictionary to the list operations
-def income(operations):
+def income(operations, balance):
     value = validate_int_over_0("Insert your income please: ")
     concept = validate_not_empty("Insert the concept of the income: ")
+    balance = balance + value
     local_dictionary = {}
     local_dictionary["value"] = value
     local_dictionary["concept"] = concept
     local_dictionary["type"] = "Income"
     operations.append(local_dictionary)
+    return balance
 
 #Function for outcome uses validations on the valitadions file and adds the values to a local dictionary to add this dictionary to the list operations
-def outcome(operations):
+def outcome(operations, balance):
     value = validate_int_over_0("Insert your outcome please: ")
     concept = validate_not_empty("Insert the concept of the outcome: ")
+    balance = balance - value
     local_dictionary = {}
     local_dictionary["value"] = -value
     local_dictionary["concept"] = concept
     local_dictionary["type"] = "outcome"
     operations.append(local_dictionary)
+    return balance
 
 
-
+for i in range (3):
+    balance=income(operations, balance)
 print(operations)
+print("Balance final: ", balance)
