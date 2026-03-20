ae35ecfa7afd411508a1cd82ceacdf2bdd851dbc
Andres-1109 <felipe11092002@gmail.com>
2026-03-19 19:54:06 -0500

    Added function to edit movements

--- src/services.py
+++ src/services.py
@@ -1,5 +1,5 @@
 # Importing validations
-from validations import validate_int_over_0, validate_not_empty
+from validations import validate_int_over_0, validate_not_empty, validate_type_of_operation
 # Creating provitional list to work the functions
 operations = []
 balance = 0
@@ -18,14 +18,17 @@ def income(operations, balance):
 # Function for outcome uses validations on the valitadions file and adds the values to a local dictionary to add this dictionary to the list operations
 def outcome(operations, balance):
     value = validate_int_over_0("Insert your outcome please: ")
-    concept = validate_not_empty("Insert the concept of the outcome: ")
-    balance = balance - value
-    local_dictionary = {}
-    local_dictionary["value"] = -value
-    local_dictionary["concept"] = concept
-    local_dictionary["type"] = "outcome"
-    operations.append(local_dictionary)
-    return balance
+    if value> balance:  
+        concept = validate_not_empty("Insert the concept of the outcome: ")
+        balance = balance - value
+        local_dictionary = {}
+        local_dictionary["value"] = -value
+        local_dictionary["concept"] = concept
+        local_dictionary["type"] = "outcome"
+        operations.append(local_dictionary)
+        return balance
+    else:
+        print("You don't have enough balance to have this outcome") 
 
 # Function to show the movements contened in the list operations
 def show_movements(operations):
@@ -34,12 +37,47 @@ def show_movements(operations):
         print(f"{index}. {list(operation.values())}")
         index+=1
 
+def edit_movements(operations, balance):
+    show_movements(operations)
+    index= len(operations)
+    while True:
+        operation_to_edit = validate_int_over_0("Insert the number of the operation to edit: ") -1
+        if operation_to_edit> index:
+            print(f"Select a number between 1 and {index}")
+        else:
+            break
+
+    type_of_operation = validate_type_of_operation("Insert 1 for income or 2 for outcome")
+    if type_of_operation ==1:
+        value = validate_int_over_0("Insert your income please: ")
+        concept = validate_not_empty("Insert the concept of the income: ")
+        current_value = operations[operation_to_edit]["value"]
+        balance = balance + value - current_value
+        operations[operation_to_edit]["value"] = value
+        operations[operation_to_edit]["concept"] = concept
+        operations[operation_to_edit]["type"] = "income"
+    else:
+        value = validate_int_over_0("Insert your outcome please: ")
+        if value> balance:  
+            concept = validate_not_empty("Insert the concept of the outcome: ")
+            balance = balance - value - current_value
+            operations[operation_to_edit]["value"] = -value
+            operations[operation_to_edit]["concept"] = concept
+            operations[operation_to_edit]["type"] = "income"
+        else:
+            print("You don't have enough balance to have this outcome, returning to menu...") 
+            return
+    return balance
+
+
+
 
 
 for i in range (2):
-    balance=outcome(operations, balance)
-print(operations)
-print("Balance final: ", balance)
+    balance=income(operations, balance)
 
-show_movements(operations)
+print(f"Total balance: ${balance}")
 
+balance = edit_movements(operations, balance)
+
+print(f"Final edited balance: ${balance}")
