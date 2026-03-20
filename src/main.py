a0743725fbc8b92e8b2b7c417c12bd06b081f193
Andrea <ahumadaborja@hotmail.com>
2026-03-20 17:41:26 -0500

    welcome

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from menu import show_menu
 def main():
     operations = []
     balance = 0
-    print("Welcome to Personal Finance Tracker!")
+    print("Welcome")
     balance = show_menu(operations, balance)
     print(f"Final balance: ${balance}")
     print("Thanks for using the app!")
